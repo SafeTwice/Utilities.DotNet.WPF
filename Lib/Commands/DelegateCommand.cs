87038f1e99567108ccbd7a824acbc5564484e7ca
Jesús González <gonzalez.jesus@safetwice.com>
2023-03-13 11:19:59 +0100

    Added DelegateCommand

---
+++ Lib/Commands/DelegateCommand.cs
@@ -0,0 +1,85 @@
+﻿/**
+ * @file
+ * @copyright  Copyright (c) 2021 SafeTwice S.L. All rights reserved.
+ * @license    See LICENSE.txt
+ */
+
+using System;
+using System.Windows.Input;
+
+namespace Utilities.WPF.Net.Commands
+{
+    /// <summary>
+    /// <see cref="ICommand"/> that delegates its execution to an action.
+    /// </summary>
+    public class DelegateCommand : ICommand
+    {
+        //===========================================================================
+        //                             PUBLIC EVENTS
+        //===========================================================================
+
+        public event EventHandler? CanExecuteChanged;
+
+        //===========================================================================
+        //                          PUBLIC CONSTRUCTORS
+        //===========================================================================
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="execute">Action to execute when the command is executed.</param>
+        public DelegateCommand( Action execute ) : this( execute, null )
+        {
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="execute">Action to execute when the command is executed.</param>
+        /// <param name="canExecute">Function that evaluates if the command can be executed.</param>
+        public DelegateCommand( Action execute, Func<bool>? canExecute )
+        {
+            m_execute = execute;
+            m_canExecute = canExecute;
+        }
+
+        //===========================================================================
+        //                            PUBLIC METHODS
+        //===========================================================================
+
+        public bool CanExecute( object? parameter )
+        {
+            if( m_canExecute == null )
+            {
+                return true;
+            }
+
+            return m_canExecute();
+        }
+
+        public void Execute( object? parameter )
+        {
+            m_execute();
+        }
+
+        /// <summary>
+        /// Notify that the value returned by <see cref="CanExecute">CanExecute()</see> has changed.
+        /// </summary>
+        /// <remarks>
+        /// Controls which state depends on the <see cref="CanExecute">CanExecute()</see> result will not update
+        /// until this method is called.
+        /// </remarks>
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke( this, EventArgs.Empty );
+        }
+
+        //===========================================================================
+        //                           PRIVATE ATTRIBUTES
+        //===========================================================================
+
+        private readonly Func<bool>? m_canExecute;
+        private readonly Action m_execute;
+    }
+
+}
