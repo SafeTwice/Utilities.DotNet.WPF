6354544572a88b549cd530f9f6c10e9d55bfa4b3
Jesús González <gonzalez.jesus@safetwice.com>
2023-05-25 15:34:22 +0200

    Added windows utility methods and attached property to enable/disable windows' close button

---
+++ Lib/Windows/WindowExtensions.cs
@@ -0,0 +1,52 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2023 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+#pragma warning disable IDE1006 // Naming Styles
+
+using System.Windows;
+
+namespace Utilities.WPF.Net.Windows
+{
+    public class WindowExtensions : DependencyObject
+    {
+        //===========================================================================
+        //                           PUBLIC PROPERTIES
+        //===========================================================================
+
+        public static readonly DependencyProperty IsCloseButtonEnabledProperty =
+            DependencyProperty.RegisterAttached( "IsCloseButtonEnabled", typeof( bool ), typeof( WindowExtensions ),
+                                                 new PropertyMetadata( true, OnIsCloseButtonEnabledChanged ) );
+
+        public static bool GetIsCloseButtonEnabled( DependencyObject obj )
+        {
+            return (bool) obj.GetValue( IsCloseButtonEnabledProperty );
+        }
+
+        public static void SetIsCloseButtonEnabled( DependencyObject obj, bool value )
+        {
+            obj.SetValue( IsCloseButtonEnabledProperty, value );
+        }
+
+        //===========================================================================
+        //                            PRIVATE METHODS
+        //===========================================================================
+
+        private static void OnIsCloseButtonEnabledChanged( DependencyObject d, DependencyPropertyChangedEventArgs e )
+        {
+            if( e.NewValue != e.OldValue )
+            {
+                var window = d as Window;
+
+                if( (bool) e.NewValue )
+                {
+                    window?.EnableCloseButton();
+                }
+                else
+                {
+                    window?.DisableCloseButton();
+                }
+            }
+        }
+    }
+}
--- Lib/Windows/WindowsUtilities.cs
+++ Lib/Windows/WindowsUtilities.cs
@@ -2,7 +2,10 @@
 /// @copyright  Copyright (c) 2023 SafeTwice S.L. All rights reserved.
 /// @license    See LICENSE.txt
 
+using System;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Interop;
 
 namespace Utilities.WPF.Net.Windows
 {
@@ -17,7 +20,7 @@ namespace Utilities.WPF.Net.Windows
 
         public static void NavigateTo( this Window fromWindow, Window window )
         {
-            window.Closed += (sender, e) =>
+            window.Closed += ( sender, e ) =>
             {
                 fromWindow.Show();
             };
@@ -25,5 +28,47 @@ namespace Utilities.WPF.Net.Windows
             fromWindow.Hide();
             window.Show();
         }
+
+        public static void DisableCloseButton( this Window window )
+        {
+            IntPtr hwnd = new WindowInteropHelper( window ).EnsureHandle();
+            IntPtr hMenu = GetSystemMenu( hwnd, false );
+
+            if( hMenu != IntPtr.Zero )
+            {
+                EnableMenuItem( hMenu, SC_CLOSE, MF_BYCOMMAND | MF_GRAYED );
+            }
+        }
+
+        public static void EnableCloseButton( this Window window )
+        {
+            IntPtr hwnd = new WindowInteropHelper( window ).EnsureHandle();
+            IntPtr hMenu = GetSystemMenu( hwnd, false );
+
+            if( hMenu != IntPtr.Zero )
+            {
+                EnableMenuItem( hMenu, SC_CLOSE, MF_ENABLED );
+            }
+        }
+
+        //===========================================================================
+        //                           PRIVATE METHODS
+        //===========================================================================
+
+        [DllImport( "user32.dll" )]
+        private static extern IntPtr GetSystemMenu( IntPtr hWnd, bool bRevert );
+
+        [DllImport( "user32.dll" )]
+        private static extern bool EnableMenuItem( IntPtr hMenu, uint uIDEnableItem, uint uEnable );
+
+        //===========================================================================
+        //                           PRIVATE CONSTANTS
+        //===========================================================================
+
+        private const uint MF_BYCOMMAND = 0x00000000;
+        private const uint MF_GRAYED = 0x00000001;
+        private const uint MF_ENABLED = 0x00000000;
+
+        private const uint SC_CLOSE = 0xF060;
     }
 }
