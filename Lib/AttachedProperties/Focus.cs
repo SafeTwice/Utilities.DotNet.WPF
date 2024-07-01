dcc18a4163b551bbaea43bf6b08993eec8f09a78
Jesús González <gonzalez.jesus@safetwice.com>
2024-07-01 19:57:57 +0200

    Add attached property to set focus to a control through a trigger

---
+++ Lib/AttachedProperties/Focus.cs
@@ -0,0 +1,76 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2020-2024 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System.Windows;
+using System.Windows.Input;
+using Utilities.DotNet.WPF.Common;
+
+namespace Utilities.DotNet.WPF.AttachedProperties
+{
+    /// <summary>
+    /// Provides an attached property to set focus on a UI element.
+    /// </summary>
+    public static class Focus
+    {
+        //===========================================================================
+        //                           PUBLIC PROPERTIES
+        //===========================================================================
+
+        /// <summary>
+        /// Dependency property to set focus on a UI element.
+        /// </summary>
+        public static readonly DependencyProperty SetFocusProperty =
+            DependencyProperty.RegisterAttached( "SetFocus", typeof( DelegateTrigger ), typeof( Focus ),
+                new FrameworkPropertyMetadata( null, OnPropertyChanged ) );
+
+        /// <summary>
+        /// Sets the value of the SetFocus property.
+        /// </summary>
+        /// <param name="obj">Dependency object on which the property value is set.</param>
+        /// <param name="value">Value to be set.</param>
+        public static void SetSetFocus( DependencyObject obj, DelegateTrigger value )
+        {
+            obj.SetValue( SetFocusProperty, value );
+        }
+
+        /// <summary>
+        /// Gets the value of the SetFocus property.
+        /// </summary>
+        /// <param name="obj">Dependency object from which the property value is obtained.</param>
+        /// <returns>Property value.</returns>
+        public static DelegateTrigger GetSetFocus( DependencyObject obj )
+        {
+            return (DelegateTrigger) obj.GetValue( SetFocusProperty );
+        }
+
+        //===========================================================================
+        //                            PRIVATE METHODS
+        //===========================================================================
+
+        private static void OnPropertyChanged( DependencyObject sender, DependencyPropertyChangedEventArgs e )
+        {
+            if( sender is UIElement uiElement )
+            {
+                if( e.NewValue is DelegateTrigger newAction )
+                {
+                    newAction.Activated += () => SetFocus( uiElement );
+                }
+            }
+        }
+        private static void SetFocus( UIElement uiElement )
+        {
+            uiElement.Dispatcher?.BeginInvoke( () =>
+            {
+                if( uiElement is IInputElement )
+                {
+                    Keyboard.Focus( uiElement );
+                }
+                else
+                {
+                    uiElement.Focus();
+                }
+            } );
+        }
+    }
+}
---
+++ Lib/Common/DelegateTrigger.cs
@@ -0,0 +1,36 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2024 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System;
+
+namespace Utilities.DotNet.WPF.Common
+{
+    /// <summary>
+    /// This class is used to activate a trigger.
+    /// </summary>
+    /// <remarks>
+    /// <para>Objects that want to be notified when the trigger is activated should subscribe to the <see cref="Activated"/> event.</para>
+    /// </para>Objects that want to activate the trigger should call the <see cref="Activate"/> method.</para>
+    /// </remarks>
+    public class DelegateTrigger
+    {
+        //===========================================================================
+        //                           PUBLIC PROPERTIES
+        //===========================================================================
+
+        public event Action? Activated;
+
+        //===========================================================================
+        //                            PUBLIC METHODS
+        //===========================================================================
+
+        /// <summary>
+        /// Activates the trigger.
+        /// </summary>
+        public void Activate()
+        {
+            Activated?.Invoke();
+        }
+    }
+}
