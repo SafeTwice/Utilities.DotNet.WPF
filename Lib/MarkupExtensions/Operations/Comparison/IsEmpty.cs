6605b9958408fd6cb92169965bc666f72c8f62df
Jesús González <gonzalez.jesus@safetwice.com>
2024-11-11 05:18:23 +0100

    Add markup extensions to check emptiness of collections

---
+++ Lib/MarkupExtensions/Operations/Comparison/IsEmpty.cs
@@ -0,0 +1,57 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2024 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System.Collections;
+using System.Windows.Markup;
+
+namespace Utilities.DotNet.WPF.MarkupExtensions
+{
+    /// <summary>
+    /// Base class for boolean binary operations.
+    /// </summary>
+    [MarkupExtensionReturnType( typeof( bool ) )]
+    public class IsEmpty : UnaryOperationBase<IEnumerable?, bool>
+    {
+        //===========================================================================
+        //                          PUBLIC CONSTRUCTORS
+        //===========================================================================
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public IsEmpty() : base( true )
+        {
+        }
+
+        /// <summary>
+        /// Constructor that initializes the operand.
+        /// </summary>
+        /// <param name="i">Operand.</param>
+        public IsEmpty( object i ) : this()
+        {
+            I = i;
+        }
+
+        //===========================================================================
+        //                            PROTECTED METHODS
+        //===========================================================================
+
+        protected override bool CalculateValue( IEnumerable? operandValue )
+        {
+            if( operandValue == null )
+            {
+                return true;
+            }
+            else
+            {
+                return !operandValue.GetEnumerator().MoveNext();
+            }
+        }
+
+        protected override IEnumerable? CalculateBackValue( bool targetValue, ComponentValue operandValue )
+        {
+            return null;
+        }
+    }
+}
---
+++ Lib/MarkupExtensions/Operations/Comparison/IsNotEmpty.cs
@@ -0,0 +1,57 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2024 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System.Collections;
+using System.Windows.Markup;
+
+namespace Utilities.DotNet.WPF.MarkupExtensions
+{
+    /// <summary>
+    /// Base class for boolean binary operations.
+    /// </summary>
+    [MarkupExtensionReturnType( typeof( bool ) )]
+    public class IsNotEmpty : UnaryOperationBase<IEnumerable?, bool>
+    {
+        //===========================================================================
+        //                          PUBLIC CONSTRUCTORS
+        //===========================================================================
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public IsNotEmpty() : base( true )
+        {
+        }
+
+        /// <summary>
+        /// Constructor that initializes the operand.
+        /// </summary>
+        /// <param name="i">Operand.</param>
+        public IsNotEmpty( object i ) : this()
+        {
+            I = i;
+        }
+
+        //===========================================================================
+        //                            PROTECTED METHODS
+        //===========================================================================
+
+        protected override bool CalculateValue( IEnumerable? operandValue )
+        {
+            if( operandValue == null )
+            {
+                return false;
+            }
+            else
+            {
+                return operandValue.GetEnumerator().MoveNext();
+            }
+        }
+
+        protected override IEnumerable? CalculateBackValue( bool targetValue, ComponentValue operandValue )
+        {
+            return null;
+        }
+    }
+}
