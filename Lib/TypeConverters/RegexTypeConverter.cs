ee66d9f008dee29389abcc02b806976601715b22
Jesús González <gonzalez.jesus@safetwice.com>
2024-12-12 17:07:14 +0100

    Add TypeConverter for Regex objects

---
+++ Lib/TypeConverters/RegexTypeConverter.cs
@@ -0,0 +1,39 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2024 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Utilities.DotNet.WPF.TypeConverters
+{
+    /// <summary>
+    /// Provides a type converter to convert <see cref="Regex"/> objects from other representations.
+    /// </summary>
+    public class RegexTypeConverter : TypeConverter
+    {
+        //===========================================================================
+        //                            PUBLIC METHODS
+        //===========================================================================
+
+        /// <inheritdoc/>
+        public override bool CanConvertFrom( ITypeDescriptorContext? context, Type sourceType )
+        {
+            return sourceType == typeof( string );
+        }
+
+        /// <inheritdoc/>
+        public override object? ConvertFrom( ITypeDescriptorContext? context, CultureInfo? culture, object value )
+        {
+            return new Regex( (string) value );
+        }
+
+        /// <inheritdoc/>
+        public override bool CanConvertTo( ITypeDescriptorContext? context, Type? destinationType )
+        {
+            return false;
+        }
+    }
+}
