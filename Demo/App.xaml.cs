07480fee09e3ab7e50e1dbde14ae321803cf8ddf
Jesús González <gonzalez.jesus@safetwice.com>
2025-08-05 10:46:01 +0200

    Add Demo application.

---
+++ Demo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Utilities.DotNet.WPF.Demo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Demo/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace Utilities.DotNet.WPF.Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+    }
+}
