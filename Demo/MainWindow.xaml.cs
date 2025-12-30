96076194827fda33bfac916ecd7987311e8c203f
Jesús González <gonzalez.jesus@safetwice.com>
2025-12-30 11:45:32 +0100

    WIP

--- Demo/MainWindow.xaml.cs
+++ Demo/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Windows;
+using System.Windows.Input;
+using Utilities.DotNet.WPF.Commands;
 
 namespace Utilities.DotNet.WPF.Demo
 {
@@ -9,11 +10,35 @@ namespace Utilities.DotNet.WPF.Demo
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public ICommand ClickCommand { get; }
+
+        public int ClickCount { get; set; }
+
+        public ICommand PastingCommand { get; }
+
         public MainWindow()
         {
             InitializeComponent();
 
             DataContext = this;
+
+            ClickCommand = new DelegateCommand( OnClick );
+
+            PastingCommand = new DelegateCommand<DataObjectPastingEventArgs>( OnPasting );
+        }
+
+        private void OnClick()
+        {
+            //MessageBox.Show( "Button clicked!" );
+            ClickCount++;
+        }
+
+        private void OnPasting( DataObjectPastingEventArgs args )
+        {
+            var text = args.DataObject.GetData( DataFormats.Text ) as string;
+
+            Dispatcher.InvokeAsync( () =>
+            MessageBox.Show( $"Pasted {text}" ) );
         }
     }
 }
